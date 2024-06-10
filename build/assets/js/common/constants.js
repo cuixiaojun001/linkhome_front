c2f8f6d8bf204f9f14e0216ce835e87cf55133e8
cuixiaojun002 <cuixiaojun002@ke.com>
2024-06-10 23:21:49 +0800

    v2.0.0

--- build/assets/js/common/constants.js
+++ build/assets/js/common/constants.js
@@ -1,6 +1,6 @@
 // 常量模块
 
 // 系统接口服务域名
-const api_domain = 'http://127.0.0.1:8080'
+// const api_domain = 'http://127.0.0.1:8080'
 // const api_domain = 'http://10.102.214.192:8080'
-// const api_domain = 'http://lb-3yneq0h7-w31v0jrp4ofbxtme.clb.ap-beijing.tencentclb.com:8080'
+const api_domain = 'http://lb-3yneq0h7-w31v0jrp4ofbxtme.clb.ap-beijing.tencentclb.com:8080'
